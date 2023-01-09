543e93c65be326ef25740757b3d612fc228ffe7b
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2023-01-09 13:57:03 -0700

    "initial attempt"

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
+//In this exercise, you will be counting from 1 - 100. For each odd number, 
+//you will print the number to the console. If the number is even, do nothing.
+
+//Create a for loop that evaluates numbers from 1 to 100.
+//Inside the loop body, write an if/else selection statement that checks whether 
+//a number is odd. If the number is odd, then print the number to the console, 
+//otherwise do nothing and continue to the next number
 console.log("EXERCISE 1:\n==========\n");
+for (let i = 0; i > 100; i++) {
+    if (i % 2 == 1) {
+    console.log(i)
+    }
+    else {
+        ;
+    }
+}
+
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
